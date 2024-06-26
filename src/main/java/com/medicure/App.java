091915c0998eec72a9bc33b2d3d08196ef40f390
praveenkumar <praveenkumar@gmail.com>
2024-06-27 00:02:15 +0530

    added

--- src/main/java/com/medicure/App.java
+++ src/main/java/com/medicure/App.java
@@ -31,8 +31,7 @@ public class App
 
       // Setup configuration
       ChromeOptions chromeOptions = new ChromeOptions();
-    //   chromeOptions.addArguments("--headless=new"); // Use the new headless mode
-    //   chromeOptions.addArguments("--disable-gpu"); // Disable GPU for headless mode
+  chromeOptions.addArguments("--headless=new"); // Use the new headless mode  chromeOptions.addArguments("--disable-gpu"); // Disable GPU for headless mode
       chromeOptions.addArguments("--no-sandbox"); // Bypass OS security model, necessary for Jenkins
       chromeOptions.addArguments("--disable-dev-shm-usage"); // Overcome limited resource problems
       chromeOptions.addArguments("--remote-debugging-port=9222"); // Add remote debugging port
